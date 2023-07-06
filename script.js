290cac3e38645e501e614858bb457bd93b953776
aryansin1105 <120688650+aryansin1105@users.noreply.github.com>
2023-07-06 14:12:58 +0530

    Dad jokes extension

---
+++ script.js
@@ -0,0 +1,6 @@
+fetch('https://icanhazdadjoke.com/slack')
+    .then(data => data.json())
+    .then(jokeData => {
+        const jokeText = jokeData.attachments[0].text;
+        document.getElementById('element').innerHTML = jokeText;
+    })
